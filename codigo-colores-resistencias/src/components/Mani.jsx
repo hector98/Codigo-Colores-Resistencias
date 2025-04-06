99ee1dc4f3bd73bcdb23bc25ac4e255bd3f60962
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2025-04-05 21:39:20 -0600

    add Mani component

---
+++ codigo-colores-resistencias/src/components/Mani.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../stylesheets/Mani.css'
+
+function Mani() {
+	return (
+		<>
+			<div className='mani'>
+				<div className='terminal'></div>
+
+				<div className='left'>
+				</div>
+
+				<div className='center'>
+				</div>
+
+				<div className='right'>
+				</div>
+
+				<div className='terminal'></div>
+			</div>
+		</>
+	)
+}
+
+export default Mani;
